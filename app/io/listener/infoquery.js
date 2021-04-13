dbab35308d7bd472bc4343ffc1010293f87e7b90
RaspberrYanagi <25424627+yana9i@users.noreply.github.com>
2021-04-13 14:50:57 +0800

    修改 infoquery 中间件

--- app/io/listener/infoquery.js
+++ app/io/listener/infoquery.js
@@ -4,19 +4,40 @@ import { ioEvent } from '../../util/dictionary.js';
 import { IoArgs } from '../../util/ioEventArgsFormatter.js';
 
 import friendshipService from '../../service/friendship.js';
+import redis from '../../util/redis.js';
 
 /** @param {import ('socket.io/dist/socket').Socket} socket  */
 export default socket => args => {
   const typeSwitch = {
     loginSuccess: async () => {
       socket.emit(ioEvent.iq, new IoArgs('selfLoginSuccess', socket.userInfo));
-      socket.broadcast.emit(ioEvent.iq, new IoArgs('loginSuccess', socket.userInfo));
       const friendlist = await friendshipService.getFriendlist({ userHostId: socket.userInfo._id });
-      socket.emit(ioEvent.iq, new IoArgs('friendlist', friendlist))
+      const friendlistOnlineSocketIds = [];
+      const friendlistWithOnline = await Promise.all(friendlist.map(async item => {
+        const socketId = await redis.get(item._id);
+        const online = socketId !== null
+        online && friendlistOnlineSocketIds.push(socketId);
+        return {
+          ...item,
+          online
+        }
+      }));
+      socket.emit(ioEvent.iq, new IoArgs('friendlist', friendlistWithOnline));
+      friendlistOnlineSocketIds.forEach(item => socket.to(item).emit(ioEvent.iq, new IoArgs('loginSuccess', { from: socket.userInfo._id })));
     },
     friendshipRequest: () => {
       console.log(args);
-    }
+    },
+    requestCryptoChat: async () => {
+      const to = await redis.get(args.payload.to);
+      console.log(to);
+      socket.to(to).emit(ioEvent.iq, args);
+    },
+    acceptCryptoChat: async () => {
+      const to = await redis.get(args.payload.to);
+      console.log(to);
+      socket.to(to).emit(ioEvent.iq, args);
+    },
   }
   typeSwitch[args.type] && typeSwitch[args.type]()
 }

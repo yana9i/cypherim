f54cec86871f52d1c36b53c1598552aa2494bc51
RaspberrYanagi <25424627+yana9i@users.noreply.github.com>
2021-04-13 14:50:25 +0800

    修改 auth 中间件

--- app/io/middleware/auth.js
+++ app/io/middleware/auth.js
@@ -2,28 +2,34 @@
 
 
 import userService from '../../service/user.js';
+import redis from '../../util/redis.js';
 
-// import crypto from 'crypto';
-// import InMemorySessionStore from '../sessionStore.js';
-// const randomId = () => crypto.randomBytes(8).toString("hex");
-// const sessionStore = InMemorySessionStore();
+/** @param {import ('socket.io/dist/index').Server}  io */
+export default io =>
 
-/**
- * @typedef {import('socket.io/dist/namespace').ExtendedError} ExtendedError
- * @param {import ('socket.io/dist/socket').Socket} socket
- * @param {(err?: ExtendedError) => void} next
- */
+  /**
+   * @typedef {import('socket.io/dist/namespace').ExtendedError} ExtendedError
+   * @param {import ('socket.io/dist/socket').Socket} socket
+   * @param {(err?: ExtendedError) => void} next
+   */
+  async (socket, next) => {
 
-export default async (socket, next) => {
+    const sessionID = socket.handshake.auth.sessionID;
+    const { username, password } = socket.handshake.auth;
 
-  const sessionID = socket.handshake.auth.sessionID;
-  const { username, password } = socket.handshake.auth;
-
-  try {
-    const userInfo = await userService.login({ username, password });
-    socket.userInfo = userInfo;
-    await next();
-  } catch (err) {
-    return await next(err);
+    try {
+      const userInfo = await userService.login({ username, password });
+      socket.userInfo = userInfo;
+      const lastSocketId = await redis.get(userInfo._id);
+      if (lastSocketId) {
+        const lastSocket = io.of('/').sockets.get(lastSocketId);
+        if (lastSocket) {
+          throw new Error("User Has Login");
+        }
+      }
+      await redis.set(userInfo._id, socket.id);
+      await next();
+    } catch (err) {
+      return await next(err);
+    }
   }
-}

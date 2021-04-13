85b480d31b465f8db441a38646609afffab9c6cf
RaspberrYanagi <25424627+yana9i@users.noreply.github.com>
2021-04-13 14:41:48 +0800

    添加自动获取壁纸

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+fetch(`http://${window.location.hostname}:3000/api/img/background?format=js&idx=0&n=6&mkt=zh-CN`).then(res => res.json()).then(res => {
+  const random = Math.round(Math.random() * 5 - 0.5);
+  const url = `https://cn.bing.com/${res.images[random].url}`;
+  const img = new Image();
+  img.src = url;
+  img.onload = () => {
+    const bgDiv = document.getElementById('bgDiv');
+    bgDiv.style.backgroundImage = `url("${url}")`;
+    bgDiv.style.animationName = 'fadein';
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>

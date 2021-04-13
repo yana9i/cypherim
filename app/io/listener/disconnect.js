47983ed78a9ae64bf6a09c89df8a423dcbd28788
RaspberrYanagi <25424627+yana9i@users.noreply.github.com>
2021-04-13 14:49:39 +0800

    修改响应 disconnected 中间件

--- app/io/listener/disconnect.js
+++ app/io/listener/disconnect.js
@@ -1,17 +1,22 @@
 'use strict';
 
-import InMemorySessionStore from '../sessionStore.js';
 
 import { ioEvent } from '../../util/dictionary.js';
+import redis from '../../util/redis.js';
 
+import { IoArgs } from '../../util/ioEventArgsFormatter.js';
+
+import friendshipService from '../../service/friendship.js';
 
 /** @param {import ('socket.io/dist/socket').Socket} socket  */
-export default socket => () => {
+export default socket => async () => {
   console.log(socket.userInfo.username + " disconnected");
-  socket.broadcast.emit(ioEvent.iq, { userID: socket.id, username: socket.username });
-  // InMemorySessionStore().saveSession(socket.sessionID, {
-  //   userID: socket.userID,
-  //   username: socket.username,
-  //   connected: false,
-  // });
+
+  const friendlist = await friendshipService.getFriendlist({ userHostId: socket.userInfo._id });
+  await Promise.all(friendlist.map(async item => {
+    const socketId = await redis.get(item._id);
+    const online = socketId !== null
+    online && socket.to(socketId).emit(ioEvent.iq, new IoArgs('logoutSuccess', { from: socket.userInfo._id }))
+  }));
+  await redis.del(socket.userInfo._id);
 }

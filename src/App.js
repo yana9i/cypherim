2da2f986bbd71b93bcbcb7006efadaefe48bef26
RaspberrYanagi <25424627+yana9i@users.noreply.github.com>
2021-04-13 14:47:09 +0800

    修改 App 组件

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import './style/App.css';
+
+
+import Chat from './components/Chat.js';
+import Login from './components/Login.js';
+import Register from './components/Register.js';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="bgDiv" />
+      <Router>
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          {/* Edit <code>src/App.js</code> and save to reload.
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">CypherIM</a>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+        <Link to="/chat">Chat</Link> */}
+
+        </header>
+        <main>
+
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/chat" component={Chat} />
+          </Switch>
+        </main>
+      </Router >
+      <ToastContainer />
     </div>
   );
 }

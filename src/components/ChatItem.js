fe57a05306b99db06eb7e63921752f42db17d0f1
RaspberrYanagi <25424627+yana9i@users.noreply.github.com>
2021-04-13 14:43:31 +0800

    新增 ChatItem 组件

---
+++ src/components/ChatItem.js
@@ -0,0 +1,35 @@
+
+function ChatItem(props) {
+
+  const avatarImg = () => {
+    if (props.avatar) {
+      return `https://localhost:3000/api/img/${props.avatar}`
+    } else {
+      const canvas = document.createElement('canvas');
+      canvas.width = 80;
+      canvas.height = 80;
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        ctx.fillStyle = "#fff";
+        ctx.font = "60px monosapce";
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'top';
+        const firstLetter = props.username.charAt(0);
+        const measure = ctx.measureText(firstLetter);
+        const height = measure.actualBoundingBoxAscent + measure.actualBoundingBoxDescent;
+        ctx.fillText(firstLetter, 40, 40 - height / 2);
+        return canvas.toDataURL();
+      }
+    }
+  }
+
+  return (
+    <div className={`chat-item ${props.selected ? 'selected-chat-item' : ''}`} onClick={() => { props.onItemClick(props._id) }}>
+      <div className={`chat-item-avatar ${props.online ? '' : 'chat-item-avatar-offline'}`} style={{ backgroundImage: `url('${avatarImg()}')` }} ></div>
+      <div className="chat-item-title">{props.username || ''}</div>
+      <div className="chat-item-message">{props.recentMsg || ""}</div>
+    </div>
+  )
+}
+
+export default ChatItem;
